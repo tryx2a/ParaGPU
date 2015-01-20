3a69a9588c85b90a47099205315b9db813a00894
Clément Linarès <clement.linares@ensimag.grenoble-inp.fr>
2015-01-20 12:39:16 +0100

    load Option et Montecarlo

--- Pricer/src/cudaLib.cu
+++ Pricer/src/cudaLib.cu
@@ -28,8 +28,20 @@ CudaLib::CudaLib(){
 */
 CudaLib::~CudaLib(){}
 
-void CudaLib::loadOption(Option* option){
-
+void CudaLib::loadOption(Option* opt){
+        cudaError_t err;
+      
+        err = cudaMalloc((void **)&(opt->T_),sizeof(float));
+        err = cudaMalloc((void **)&(opt->timeSteps_),sizeof(int));
+        err = cudaMalloc((void **)&(opt->size_),sizeof(int));
+        
+        err = cudaMalloc((void **)&( (dynamic_cast<OptionBasket*>(opt))->strike_ ),sizeof(float));
+        
+        int sizePayoffCoeff = (dynamic_cast<OptionBasket*>(opt))->payoffCoeff_->size;
+        double *arrayPayoffCoeff = (dynamic_cast<OptionBasket*>(opt))->payoffCoeff_->array;
+        err = cudaMalloc((void **)&(sizePayoffCoeff),sizeof(int));
+        err = cudaMalloc((void **)arrayPayoffCoeff ,sizeof(float)*sizePayoffCoeff);
+     
 }
 
 void CudaLib::loadBS(BS* bs){
@@ -37,5 +49,11 @@ void CudaLib::loadBS(BS* bs){
 }
 
 void CudaLib::loadMonteCarlo(MonteCarlo* mc){
-
+        loadOption(mc->opt_);
+        loadBS(mc->mod_);
+        
+        cudaError_t err;
+        err = cudaMalloc((void **)&(this->h_),sizeof(float));
+        err = cudaMalloc((void **)&(this->H_),sizeof(int));
+        err = cudaMalloc((void **)&(this->samples_),sizeof(int));
 }
